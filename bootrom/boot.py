1cb0c87e411cce81d5fc309fff2484d44ca4960c
Jiajie Chen <c@jia.je>
2021-09-28 10:20:50 +0800

    Add boot via serial script

---
+++ bootrom/boot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import struct
+import serial
+import select
+import time
+import getopt
+
+try:
+    optlist, args = getopt.getopt(sys.argv[1:], 's')
+
+    timeout = 0.01
+    n = 1024
+    slow = False
+
+    for o, a in optlist:
+        if o == "-s":
+            slow = True
+            n = 16
+            print('Running in slow mode')
+
+    out = serial.Serial(args[1], 115200, timeout=timeout)
+
+    size = os.path.getsize(args[0])
+    out.write(struct.pack('>I', size))
+    with open(args[0], 'rb') as f:
+        data = f.read()
+        for i in range(0, len(data), n):
+            out.write(data[i:i+n])
+            print('.', end='', flush=True)
+            if slow:
+                time.sleep(timeout)
+
+    out.close()
+    os.execlp('screen', 'screen', args[1], '115200')
+except getopt.GetoptError as err:
+    print(str(err))
+    print('Usage: send.py [-s] file tty')
